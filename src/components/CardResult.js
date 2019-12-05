866c68e692ce2005a75bea93f8b63a703cc14aa8
Matteo <bernabeimatteo4@gmail.com>
2019-12-05 09:42:40 +0100

    The map can render multiple results now

--- src/components/CardResult.js
+++ src/components/CardResult.js
@@ -5,28 +5,18 @@ class CardResult extends React.Component {
   render() {
     const { results } = this.props;
 
-    // console.log(results[0].id);
+    const treeResults = [3, 5];
+    const arrResult = [];
 
-    const fakeResult = [
-      {
-        id: 1,
-        title: "Tieni in ordine il prato con il tagliaerba",
-        description:
-          "Il tagliaerba è il tuo miglior alleato per prenderti cura e tenere sempre in ordine il tuo spazio verde. Per svolgere un buon lavoro in breve tempo e con poco sforzo, scegli la tipologia giusta considerando le dimensioni del tuo giardino, il tipo di terreno e l’alimentazione più adatta.",
-        url:
-          "https://www.leroymerlin.it/prodotti/tagliaerba-CAT749-c?ruleFilter=FRULE66412%2CFRULE1920675"
-      },
-      {
-        id: 5,
-        title: "Tieni in ordine il prato con il tagliaerba",
-        description:
-          "Il tagliaerba è il tuo miglior alleato per prenderti cura e tenere sempre in ordine il tuo spazio verde. Per svolgere un buon lavoro in breve tempo e con poco sforzo, scegli la tipologia giusta considerando le dimensioni del tuo giardino, il tipo di terreno e l’alimentazione più adatta.",
-        url:
-          "https://www.leroymerlin.it/prodotti/tagliaerba-CAT749-c?ruleFilter=FRULE1269%2CFRULE1306"
-      }
-    ];
+    treeResults.map(treeResult =>
+      results.map(res => {
+        if (res.id === treeResult) {
+          arrResult.push(res);
+        }
+      })
+    );
 
-    const resultMap = fakeResult.map(res => (
+    const resultMap = arrResult.map(res => (
       <div className="step-card" key={res.id}>
         <h3>{res.title}</h3>
         <p>{res.description}</p>
@@ -38,14 +28,7 @@ class CardResult extends React.Component {
       </div>
     ));
 
-    return (
-      <div>
-        <div>
-          <h1>{"This will always render"}</h1>
-        </div>
-        {resultMap}
-      </div>
-    );
+    return <div>{resultMap}</div>;
   }
 }
 
